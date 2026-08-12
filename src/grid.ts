eedfd3c115cd39d712d14061e77529f8a3c9e0b4
danielgstephenson <dgstephenson.econ@gmail.com>
2026-08-12 16:42:17 -0400

    grid

--- src/grid.ts
+++ src/grid.ts
@@ -1,12 +1,35 @@
-import { G } from "@svgdotjs/svg.js";
+import { G, Rect } from "@svgdotjs/svg.js";
 import type { Renderer } from "./renderer";
+import { range } from "./math";
+import { borderColor, gridSize, highlightColor } from "./parameters";
 
 export class Grid {
   renderer: Renderer
   group: G
+  tiles: Rect[][] = []
+  highlights: Rect[][] = []
 
   constructor (renderer: Renderer) {
     this.renderer = renderer
     this.group = renderer.svg.group()
+    range(gridSize).forEach(x => {
+      this.tiles[x] = []
+      this.highlights[x] = []
+      range(gridSize).forEach(y => {
+        const highlight = this.group.rect(1, 1).center(x, y)
+        highlight.stroke({
+          color: highlightColor,
+          width: 0.07,
+          linecap: 'square'
+        })
+        highlight.fill('none')
+        highlight.opacity(0)
+        this.highlights[x][y] = highlight
+        const tile = this.group.rect(1, 1).center(x, y)
+        tile.stroke({ color: borderColor, width: 0.05 })
+        tile.fill('none')
+        this.tiles[x][y] = tile
+      })
+    })
   }
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,15 +1,30 @@
-import { SVG } from '@svgdotjs/svg.js'
+import { SVG} from '@svgdotjs/svg.js'
 import * as opentype from 'opentype.js'
+import { gridSize } from './parameters'
+import { Grid } from './grid'
 
 export class Renderer {
   font: opentype.Font
+  svgDiv = document.getElementById('svgDiv') as HTMLDivElement
   svg = SVG().addTo('#svgDiv')
-  setupComplete = false
+  padding = 1.25
+  grid: Grid
 
   constructor (font: opentype.Font) {
     this.font = font
     this.onResize()
     window.addEventListener('resize', () => this.onResize())
+    this.setup()
+    this.grid = new Grid(this) 
+  }
+
+  setup() {
+    const x = -0.5 - this.padding
+    const y = -0.5 - this.padding
+    const width = gridSize + 2 * this.padding
+    const height = gridSize + 2 * this.padding
+    this.svg.flip('y')
+    this.svg.viewbox(x, y, width, height)
   }
 
   onResize (): void {
